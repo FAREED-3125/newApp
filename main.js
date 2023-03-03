064969f792c0988d2e9cae9491ad02bb79fb86fe
FAREED-3125 <126556539+FAREED-3125@users.noreply.github.com>
2023-03-03 23:07:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function openMenu(){
+    document.getElementById("mobile-nav-bar").classList.toggle("onclick-nav-bar");
+}
+function closeMenu(){
+    document.getElementById("mobile-nav-bar").classList.remove("onclick-nav-bar");
+}
